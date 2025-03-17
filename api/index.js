748f77051a603ee310d2eff1245a8d511e0567eb
Rosendo Rodriguez <chendo1988@protonmail.com>
2025-03-16 23:48:12 -0600

    Final changes

--- api/index.js
+++ api/index.js
@@ -22,10 +22,6 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.json({ message: "API is running..." });
-})
-
 app.use('/api/auth', authRoutes)
 app.use('/api/users', usersRoutes)
 app.use('/api/posts', postsRoutes)
